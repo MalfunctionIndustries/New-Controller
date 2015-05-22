f7a04998502337a98fc9cebdc1aa756657c91540
Oscar Louw Laptop <ojlou1@student.monash.edu>
2015-05-22 13:24:39 +1000

    Changed EVERYTHING
    - Fixed a bunch of bugs
    - Moving platform system
    - Lots of other stuff

---
+++ 2D Platformer 4/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+
+    public GameObject door;
+
+    private bool open = false;
+    private Vector2 initialScale;
+
+    void Start()
+    {
+        initialScale = door.transform.localScale;
+    }
+
+	// Update is called once per frame
+	public void toggleDoor () {
+
+        if(!open)
+        {
+            open = true;
+            Debug.Log("OPENED");
+            door.transform.localScale = Vector2.zero;
+        }
+        else
+        {
+            open = false;
+            Debug.Log("CLOSED");
+            door.transform.localScale = initialScale;
+        }
+	}
+}
---
+++ 2D Platformer 4/Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreCollision : MonoBehaviour {
+
+    public Collider2D[] IgnoreList;
+
+	// Use this for initialization
+	void Start () {
+
+        for(int i = 0; i<IgnoreList.Length; i++)
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), IgnoreList[i]);
+        }
+
+	}
+
+}
